8e4c6ba5ff7a0774e31351fb248ba09b5b529848
hikarl1 <83234547+hikarl1@users.noreply.github.com>
2025-03-06 17:40:44 +0700

    Update  komen BankAccountMain.java

--- BankAccountMain.java
+++ BankAccountMain.java
@@ -14,15 +14,15 @@ class BankAccountMain {
         BankAccount AAA = new BankAccount("12345678", "Abecede Eefgeha", "Tabungan");
         BankAccount BBB = new BankAccount("45678901", "Ijekael Emeno", "Giro");
         
-        AAA.displayInfo();
-        AAA.deposit(200.0);
-        AAA.withdraw(150.0);
-        AAA.transfer(BBB, 50.0);
+        AAA.displayInfo(); //menampilkan informasi akun AAA
+        AAA.deposit(200.0); //melakukan deposit sebesar 200, saldo bertambah 200 dari semula
+        AAA.withdraw(150.0); // melakukan withdraw sebesar 150, saldo berkurang 150
+        AAA.transfer(BBB, 50.0); //melakukan transfer ke BBB sebesar 50, saldo berkurang 50, saldo BBB bertambah 50
                 
-        BBB.displayInfo();
-        BBB.deposit(100.0);
-        BBB.withdraw(300.0);
-        BBB.transfer(AAA, 75.0);
+        BBB.displayInfo(); //menampilkan informasi akun BBB
+        BBB.deposit(100.0); //melakukan deposit sebesar 100, saldo bertambah 100 dari semula
+        BBB.withdraw(300.0); // melakukan withdraw sebesar 300, saldo berkurang 300
+        BBB.transfer(AAA, 75.0); //melakukan transfer ke AAA sebesar 75, saldo berkurang 75
 
     }
 }
